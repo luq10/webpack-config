51ddf7fb642bee3e48089b1c82b6cacd26b79337
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2017-10-18 21:17:12 +0200

    Add hot realod on css files

--- src/test.js
+++ src/test.js
@@ -4,6 +4,19 @@ import './assets/styles/index.scss';
 const img = document.createElement('img');
 img.src = imagePath;
 
+let i = 0;
+
+const button = document.createElement('button');
+button.innerText = 'click';
+button.addEventListener('click', function(){
+	const span = document.createElement('span');
+	span.innerText = ++i;
+
+	document.body.appendChild(span);
+});
+
+document.body.appendChild(button);
+
 // Some comments
 console.log('test');
 console.log(process.env.NODE_ENV);
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,32 @@
 const path = require('path');
 const webpack = require('webpack');
-const Merge = require('webpack-merge');
+const merge = require('webpack-merge');
 
 const CommonConfig = require('./webpack.common.js');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const config = Merge(CommonConfig, {
+const config = merge(CommonConfig, {
     devtool: 'cheap-module-source-map',
 	devServer: {
 		publicPath: path.resolve(__dirname, '/'),
 		// Server can be visible also by your IP address in LAN
 		host: '0.0.0.0',
-		port: 3000
+		port: 3000,
+		hot: true
 	},
+	output: merge(CommonConfig.output, {
+		// Cannot use [chunkhash] for chunk when use HMR
+		filename: '[name].[hash].js'
+	}),
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
 				'NODE_ENV': JSON.stringify('development')
 			}
 		}),
+
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
 
 		// Copy app.config to /dist folder.
 		//
