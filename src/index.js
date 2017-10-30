7c0e053ace9c6c68f08de042655669b9fa883708
Daniel Chmielowiec <daniel.chmielowiec@jazzy.pro>
2017-10-30 11:55:52 +0100

    Added Typescript support, moved config from root to src/ directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import sum from './sum';
-import appConfig from 'appConfig';
-import './test';
-
-console.log(sum(100, 2000));
-console.log(appConfig);
--- src/sum.js
+++
@@ -1,6 +0,0 @@
-import {lodash} from 'lodash';
-import {underscore} from 'underscore';
-
-const sum = (a, b) => a + b;
-
-export default sum;
