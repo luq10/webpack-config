690b173e93e8f7e956409abb78c2717f7c45d7a1
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2020-02-13 15:09:43 +0100

    Add support for CSS Module in storybook, import index.scss in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = async ({ config }) => {
   config.module.rules.push({
     test: /\.scss$/,
+    exclude: /\.module\.scss$/,
     use: [
       'style-loader',
       'css-loader',
@@ -15,6 +16,28 @@ module.exports = async ({ config }) => {
         },
       },
     ],
+  });
+
+  config.module.rules.push({
+    test: /\.module\.scss$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          sourceMap: true,
+        },
+      },
+      'sass-loader',
+      // Share SASS variables, mixins and functions with all .sass files
+      {
+        loader: 'sass-resources-loader',
+        options: {
+          resources: path.resolve(__dirname, '../src/assets/styles/sass-resources.scss'),
+        },
+      },
+    ]
   });
 
   return config;
